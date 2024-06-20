859b53786cda6e955b19047e5b85f524fcb4d10b
aszulcc <aszulc9@st.swps.edu.pl>
2024-06-20 17:06:52 +0200

    text align center

--- sketch.js
+++ sketch.js
@@ -93,6 +93,7 @@ function draw() {
    if (chapter == 0) { //ekran przed kwiatkiem
     background(0);
     fill(255);
+    textAlign(CENTER);
     text('level 3 Bargaining',1920/2,1080/2, 300);
     
   }
